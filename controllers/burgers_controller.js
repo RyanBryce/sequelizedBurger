5934a1003e105e6d15e4443280ea92b59a616a76
Papa Deere <papadeere5150@gmail.com>
2018-02-23 11:42:20 -0700

    updated

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,11 +3,11 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
-var burger = require("../models/burger.js");
+var db = require("../models");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function (req, res) {
-    burger.selectAll(function (data) {
+    db.burger.findAll({}).then(function (data) {
         var hbsObject = {
             burgers: data
         };
@@ -17,18 +17,28 @@ router.get("/", function (req, res) {
 });
 
 router.post("/api/burgers", function (req, res) {
-    burger.create(["burger_name"], [req.body.burger_name], function (data) {
+
+    db.burger.create({
+        burger_name: req.body.burger_name
+    }).then(function (data) {
         // Send back the ID of the new quote
         res.redirect('/');
     });
 });
 
 router.put("/api/burgers/:id", function (req, res) {
-    var condition = "id = " + req.params.id;
+    var id = req.params.id;
 
-    console.log("condition", condition);
+    // console.log("condition", condition);
 
-    burger.update({ devoured: 1 }, condition, function (data) {
+    db.burger.update({ 
+        devoured: true 
+    }, {
+        where:{
+            id: id
+        }
+    }
+    ).then(function (data) {
         res.redirect('/');
     });
 });
